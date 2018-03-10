5dcc70b7e431da208aded4a0c89a14e57471392a
Rohan M. Nanaware <rohan.nanaware@outlook.com>
2018-03-10 23:57:57 +0530

    Add files via upload

---
+++ Chatbot/2. create_training_data.py
@@ -0,0 +1,38 @@
+import sqlite3
+import pandas as pd
+
+timeframes = ['2006-01']
+
+for timeframe in timeframes:
+    connection = sqlite3.connect('{}.db'.format(timeframe))
+    c = connection.cursor()
+    limit = 5000# how many rows are being put at once in the pandas dataframe
+    last_unix = 0# to help buffer through the data
+    cur_length = limit
+    counter = 0
+    test_done = False
+    while cur_length == limit:
+        df = pd.read_sql("SELECT * FROM parent_reply WHERE unix > {} AND parent NOT NULL AND score > 0 ORDER BY unix ASC LIMIT {}".format(last_unix, limit), connection)
+        last_unix = df.tail(1)['unix'].values[0]
+        cur_length = len(df)
+        if not test_done:
+            with open("test.from",'a', encoding = 'utf8') as f:
+                for content in df['parent'].values:
+                    f.write(content+'\n')
+            with open("test.to",'a',encoding = 'utf8') as f:
+                for content in df['comment'].values:
+                    f.write(content+'\n')
+            test_done = True
+
+        else:
+            with open("train.from",'a', encoding = 'utf8') as f:
+                for content in df['parent'].values:
+                    f.write(content+'\n')
+            with open("train.to",'a',encoding = 'utf8') as f:
+                for content in df['comment'].values:
+                    f.write(content+'\n')
+            test_done = True
+
+        counter += 1
+        if counter % 20 == 0:
+            print(counter*limit, 'rows completed so far')
---
+++ Chatbot/chatbot_database.py
@@ -0,0 +1,165 @@
+'''
+Author - Rohan M. Nanaware
+Date C.- 10th Mar 2018
+Date M.- 10th Mar 2018
+Purpose- Learn to build a chatbot
+Updates-
+    1. Replicating the codes from sentdex channel
+
+Ref.   -
+    http://files.pushshift.io/reddit/comments/
+'''
+
+import sqlite3
+import json
+from datetime import datetime
+
+# this will be a list of time frames later when training on larger data
+timeframe = '2006-01'
+sql_transaction = []
+start_row = 0
+cleanup = 1000000
+
+# setup sqlite connection to the data
+connection = sqlite3.connect('{}.db'.format(timeframe))
+c = connection.cursor()
+
+# function to crete empty table where data will be stored in
+def create_table():
+    c.execute("CREATE TABLE IF NOT EXISTS parent_reply(parent_id TEXT PRIMARY KEY, comment_id TEXT UNIQUE, parent TEXT, comment TEXT, subreddit TEXT, unix INT, score INT)")
+
+# replace new line characters with text
+def format_data(data):
+    data = data.replace('\n',' newlinechar ').replace('\r',' newlinechar ').replace('"',"'")
+    return data
+
+# pass the sql queries through the connection
+def transaction_bldr(sql):
+    global sql_transaction
+    sql_transaction.append(sql)
+    if len(sql_transaction) > 1000:
+        c.execute('BEGIN TRANSACTION')
+        for s in sql_transaction:
+            try:
+                c.execute(s)
+            except:
+                pass
+        connection.commit()
+        sql_transaction = []
+
+# insert data into tables - # havent understood what the 1st function is doing, why "?"
+def sql_insert_replace_comment(commentid,parentid,parent,comment,subreddit,time,score):
+    try:
+        sql = """UPDATE parent_reply SET parent_id = ?, comment_id = ?, parent = ?, comment = ?, subreddit = ?, unix = ?, score = ? WHERE parent_id =?;""".format(parentid, commentid, parent, comment, subreddit, int(time), score, parentid)
+        transaction_bldr(sql)
+    except Exception as e:
+        print('s-UPDATE insertion',str(e))
+
+def sql_insert_has_parent(commentid,parentid,parent,comment,subreddit,time,score):
+    try:
+        sql = """INSERT INTO parent_reply (parent_id, comment_id, parent, comment, subreddit, unix, score) VALUES ("{}","{}","{}","{}","{}",{},{});""".format(parentid, commentid, parent, comment, subreddit, int(time), score)
+        transaction_bldr(sql)
+    except Exception as e:
+        print('s-PARENT insertion',str(e))
+
+def sql_insert_no_parent(commentid,parentid,comment,subreddit,time,score):
+    try:
+        sql = """INSERT INTO parent_reply (parent_id, comment_id, comment, subreddit, unix, score) VALUES ("{}","{}","{}","{}",{},{});""".format(parentid, commentid, comment, subreddit, int(time), score)
+        transaction_bldr(sql)
+    except Exception as e:
+        print('s-NO PARENT insertion',str(e))
+
+# apply conditions for acceptable data
+def acceptable(data):
+    if len(data.split(' ')) > 1000 or len(data) < 1:
+        return False
+    elif len(data) > 32000:
+        return False
+    elif data == '[deleted]':
+        return False
+    elif data == '[removed]':
+        return False
+    else:
+        return True
+
+# find the text in parent comment of any comment
+def find_parent(pid):
+    try:
+        sql = "SELECT comment FROM parent_reply WHERE comment_id = '{}' LIMIT 1".format(pid)
+        c.execute(sql)
+        result = c.fetchone()
+        if result != None:
+            return result[0]
+        else: return False
+    except Exception as e:
+        #print(str(e))
+        return False
+
+# find the score of the comment with given parent id
+# returns score for the first comment it finds, should this be sorted by score before fetching?
+def find_existing_score(pid):
+    try:
+        sql = "SELECT score FROM parent_reply WHERE parent_id = '{}' LIMIT 1".format(pid)
+        c.execute(sql)
+        result = c.fetchone()
+        if result != None:
+            return result[0]
+        else: return False
+    except Exception as e:
+        #print(str(e))
+        return False     
+
+# read the comments and fetch individual columns
+if __name__ == '__main__':
+    create_table()
+    row_counter = 0
+    paired_rows = 0
+
+    with open('E:/Delivery/1 Active/Python/Chatbot/RC_{}'.format(timeframe), buffering=1000) as f:
+    #with open('/home/paperspace/reddit_comment_dumps/RC_{}'.format(timeframe), buffering=1000) as f:
+        for row in f:
+            #print(row)
+            #time.sleep(555)
+            row_counter += 1
+
+            if row_counter > start_row:
+                try:
+                    row = json.loads(row)
+                    parent_id = row['parent_id'].split('_')[1]
+                    body = format_data(row['body'])
+                    created_utc = row['created_utc']
+                    score = row['score']
+                    
+                    comment_id = row['id']
+                    
+                    subreddit = row['subreddit']
+                    parent_data = find_parent(parent_id)
+                    
+                    existing_comment_score = find_existing_score(parent_id)
+                    if existing_comment_score:
+                        if score > existing_comment_score:
+                            if acceptable(body):
+                                sql_insert_replace_comment(comment_id,parent_id,parent_data,body,subreddit,created_utc,score)
+                                
+                    else:
+                        if acceptable(body):
+                            if parent_data:
+                                if score >= 2:
+                                    sql_insert_has_parent(comment_id,parent_id,parent_data,body,subreddit,created_utc,score)
+                                    paired_rows += 1
+                            else:
+                                sql_insert_no_parent(comment_id,parent_id,body,subreddit,created_utc,score)
+                except Exception as e:
+                    print(str(e))
+                            
+            if row_counter % 100000 == 0:
+                print('Total Rows Read: {}, Paired Rows: {}, Time: {}'.format(row_counter, paired_rows, str(datetime.now())))
+
+            if row_counter > start_row:
+                if row_counter % cleanup == 0:
+                    print("Cleanin up!")
+                    sql = "DELETE FROM parent_reply WHERE parent IS NULL"
+                    c.execute(sql)
+                    connection.commit()
+                    c.execute("VACUUM")
+                    connection.commit()
