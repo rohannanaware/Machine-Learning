0af0267b932ac224a79c54fd4e02482adb7800fc
Rohan M. Nanaware <rohan.nanaware@outlook.com>
2018-03-26 13:43:08 +0530

    Update opencv6-thresholding.py

--- Object detection/Sentdex/OpenCV/opencv6-thresholding.py
+++ Object detection/Sentdex/OpenCV/opencv6-thresholding.py
@@ -6,7 +6,7 @@ retval, threshold = cv2.threshold(img, 12, 255, cv2.THRESH_BINARY)
 
 grayscaled = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 retval2, threshold2 = cv2.threshold(grayscaled, 12, 255, cv2.THRESH_BINARY)
-##we observe that grayscaling followed by thresholding removes few details whereing the image is a dark or the threshold limits are strained
+##we observe that grayscaling followed by thresholding removes few details wherein the image is a dark or the threshold limits are strained
 ##hence we use a type of threshold called Gaussian adaptive threshold
 gaus = cv2.adaptiveThreshold(grayscaled, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1)
 retval3, otsu = cv2.threshold(grayscaled, 125, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
