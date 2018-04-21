53adb704bd718e48af9fdfcbbcd8b0cac3addfab
Rohan M. Nanaware <rohan.nanaware@outlook.com>
2018-04-21 13:38:14 +0530

    Update caavo-cv.py

--- HackerEarth/Caavo Computer Vision Challenge/caavo-cv.py
+++ HackerEarth/Caavo Computer Vision Challenge/caavo-cv.py
@@ -1,6 +1,7 @@
 import cv2
 import tensorflow as tf
 import os
+import pandas as pd
 
 # print the working directory
 print('Current working directory : ', os.getcwd())
@@ -31,5 +32,27 @@ list_class_dct = {'1':'Blouse',
 				  '15':'Waistcoat'}
 print('Checking if the dictionary was defined properly : ', list_class_dct['11'])
 
-for key, class_str in list_class_dct:
-	print('{} images of {} class'.format(len(os.listdir(os.path.join))))
+for class_num, class_str in list_class_dct:
+	print('{} images of {} class'.format(len(os.listdir(os.path.join(train_dir, class_num))), class_str))
+
+# create a lookup file for the train & test data
+train = []
+for class_num, class_str in list_class_dct:
+	for file in os.listdir(os.path.join(train_dir, class_num)):
+		train.append(['train/{}/{}'.format(class_num, file), file, class_num, class_str])
+
+train_df = pd.DataFrame(train, columns = ['filepath', 'file', 'class_num', 'class_str'])
+print('Size of train data : ', train_df.shape)
+
+test = []
+for file in os.listdir(test_dir):
+	test.append(['test/{}'.format(file), file])
+
+test_df = pd.DataFrame(test, columns = ['filepath', 'file'])
+print('Size of test data : ', test_df.shape)
+
+
+
+
+
+
