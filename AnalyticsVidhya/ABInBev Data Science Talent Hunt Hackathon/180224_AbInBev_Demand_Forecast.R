23c501ee8da249bb27b39fc2d8804ca6d09bbf0b
Rohan M. Nanaware <rohan.nanaware@outlook.com>
2018-07-02 01:47:04 +0530

    Update 180224_AbInBev_Demand_Forecast.R

--- AnalyticsVidhya/ABInBev Data Science Talent Hunt Hackathon/180224_AbInBev_Demand_Forecast.R
+++ AnalyticsVidhya/ABInBev Data Science Talent Hunt Hackathon/180224_AbInBev_Demand_Forecast.R
@@ -1,10 +1,14 @@
 #Author : Rohan M. Nanaware
 #Date C.: 24th Feb 2018
-#Date M.: 24th Feb 2018
+#Date M.: 02nd Jul 2018
 #Purpose: Demand Forecast Model for AbInBev - https://datahack.analyticsvidhya.com/contest/data-science-talent-hunt-hackathon/lb
+#             a. Forecast the demand volume for Jan’18 of all agency-SKU combination
 #Updates:
 {
-  
+  # 02nd Jul 2018 :
+  #   1. Updates to module 3
+  #       a. Added summary for historical volume data
+  #       !  Read up more on ggplot
 }
 {
   #Time series forecasting
@@ -15,6 +19,7 @@
   library(data.table)
   library(xgboost)
   library(dplyr)
+  library(ggplot2)
 }#01. Load required packages and helper functions
 {
   wd <- "E:/Delivery/1 Active/AnalyticsVidhya/ABInBev Data Science Talent Hunt Hackathon"
@@ -47,25 +52,31 @@
                                     header = T, 
                                     stringsAsFactors = F,
                                     na.strings = c(NA, "NA","")))
-  }#02. Data import
+}#02. Data import
 {
-  
-  summary(price_sales_promotion)
-  quantile(price_sales_promotion$Price, probs = c(0,
-                                                  0.05,
-                                                  0.25,
-                                                  0.5,
-                                                  0.75,
-                                                  0.95,
-                                                  1))
-  summary(historical_volume)#outlier treatment
-  summary(weather)
-  summary(industry_soda_sales)
-  summary(event_calendar)
-  summary(industry_volume)#need moere details on what the volumes in this dataset mean
-  summary(demographics)
+  # Historical volumes data
+  summary(historical_volume)
+  sapply(historical_volume, function(x) sum(is.na(x)))
   
 }#03. Data checks
 {
   
-}#04. Creating train data
+  {
+    
+    historical_volume <- as.data.table(historical_volume)
+    
+    # read up more on ggplot
+    ggplot(data = historical_volume, aes(x = YearMonth, y = Volume, group = 1)) + 
+      geom_line() + 
+      geom_point()
+    
+    volume_forecast <- historical_volume[historical_volume$YearMonth == '201712']
+    volume_forecast$YearMonth <- NULL
+    write.csv(volume_forecast, '180702_volume_forecast_v1.csv', row.names = F)
+    
+    # Results - The historical volume data has sales only for select few SKUs within each agency, meaning all SKUs are not sold in every agency
+    #           In such scenario how do we go about predicting sales for products that are not sold in that Agency?
+      
+  }# Method 1 - Forecast for 2018 Jan is the same as Dec 2017, Jan 2017
+  
+}#04. Approach 1 - Use only historical volume to forecast demand
