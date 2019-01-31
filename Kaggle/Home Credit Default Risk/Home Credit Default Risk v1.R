7bb57bce95d9a2ff9c30273eaab5277b3e93abab
Rohan M. Nanaware <rohan.nanaware@outlook.com>
2019-01-31 22:10:55 +0530

    Update Home Credit Default Risk v1.R

--- Kaggle/Home Credit Default Risk/Home Credit Default Risk v1.R
+++ Kaggle/Home Credit Default Risk/Home Credit Default Risk v1.R
@@ -1,16 +1,18 @@
 #########################################################
 # Import required libraries
 #########################################################
+
 library(data.table)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
+library(grid)
 
 #########################################################
 # Reference links
 #########################################################
 
-# - Kaggle HorT kernel : https://www.kaggle.com/headsortails/steering-wheel-of-fortune-porto-seguro-eda
+# - Kaggle H | T kernel : https://www.kaggle.com/headsortails/steering-wheel-of-fortune-porto-seguro-eda
 
 #########################################################
 # Helper functions
@@ -30,34 +32,34 @@ get_binCI <- function(x,n) as.list(setNames(binom.test(x,n)$conf.int, c("lwr", "
 # 3 will go all the way across the bottom.
 #
 multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
-
+  
   # Make a list from the ... arguments and plotlist
   plots <- c(list(...), plotlist)
-
+  
   numPlots = length(plots)
-
+  
   # If layout is NULL, then use 'cols' to determine layout
   if (is.null(layout)) {
     # Make the panel
     # ncol: Number of columns of plots
     # nrow: Number of rows needed, calculated from # of cols
     layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
-                    ncol = cols, nrow = ceiling(numPlots/cols))
+                     ncol = cols, nrow = ceiling(numPlots/cols))
   }
-
- if (numPlots==1) {
+  
+  if (numPlots==1) {
     print(plots[[1]])
-
+    
   } else {
     # Set up the page
     grid.newpage()
     pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
-
+    
     # Make each plot, in the correct location
     for (i in 1:numPlots) {
       # Get the i,j matrix positions of the regions that contain this subplot
       matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
-
+      
       print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
                                       layout.pos.col = matchidx$col))
     }
@@ -68,124 +70,63 @@ multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
 # Load data
 #########################################################
 
-list.files('../input/')
+# get a list of files in input folder
+# list.files('../input')
+# application_train <- fread('../input/application_train.csv', header = T, stringsAsFactors = F)
+application_train <- fread('application_train.csv', header = T, stringsAsFactors = F)
 
-# [1] "application_test.csv" "application_train.csv" [3] "bureau_balance.csv" "bureau.csv" [5] "credit_card_balance.csv" "installments_payments.csv" [7] "POS_CASH_balance.csv" "previous_application.csv" [9] "sample_submission.csv"
+#########################################################
+# eda
+#########################################################
 
-application_train <- fread('../input/application_train.csv', header = T, stringsAsFactors = F)
+# group features by data type
+table(sapply(application_train, function(x) class(x)))
+# character   integer   numeric 
+# 16        41        65 
+# filter all character columns
+# application_train <- data.frame(application_train)
+# application_train_character = application_train[,c(as.integer(sapply(application_train, function(x) is.character(x))))]
+application_train_character <- application_train %>%
+  select_if(is.character)
+summary(application_train_character)
+# NA count
+na_count = round(sapply(application_train_character, function(x) sum(is.na(x)))/nrow(application_train_character)*100, 0)
+if (sum(na_count) > 0) {
+  print("Columns with NAs")
+  print(na_count[na_count > 0])
+} else {
+  print(paste("NAs absent in", "character type", "features"))
+}
 
-# Univariates
-table(application_train$TARGET)
-# Overall default rate ~8%
+application_train_character <- cbind(application_train_character, "TARGET" = application_train$TARGET)
 
-# Bivariates
+for (column in colnames(application_train_character)[1:ncol(application_train_character) - 1]) {
+  
+  assign(paste0("plot_", "char_", column), eval(parse(text = paste("application_train_character %>%
+                                                                    group_by(" , column , ",TARGET) %>%
+                                                                    count() %>%
+                                                                    spread(TARGET, n) %>%
+                                                                    mutate(default_rate = `1`/(`1` + `0`)*100,
+                                                                    lwr = tryCatch(get_binCI(`1`, (`1` + `0`))[[1]]*100,
+                                                                    warning = function(w) {0},
+                                                                    error   = function(e) {0}),
+                                                                    upr = tryCatch(get_binCI(`1`, (`1` + `0`))[[2]]*100,
+                                                                    warning = function(w) {0},
+                                                                    error   = function(e) {0})) %>%
+                                                                    ggplot(aes(x =",column , ", y = default_rate, fill =" , column , paste0(")) +
+                                                                    # theme(legend.position = ", "none", ") +
+                                                                    geom_col()
+                                                                    # + geom_errorbar(aes(ymin = lwr, ymax = upr))"
+                                                                    # + labs(y = ","Default rate [%]",")"
+                                                                    )))))
+}
 
-## Categorical features
-
-# NAME_CONTRACT_TYPE
-p1 = application_train %>%
-    group_by(NAME_CONTRACT_TYPE, TARGET) %>%
-    count() %>%
-    spread(TARGET, n) %>%
-    mutate(frac_def = `1`/(`1`+`0`)*100,
-            lwr = get_binCI(`1`,(`1`+`0`))[[1]]*100,
-            upr = get_binCI(`1`,(`1`+`0`))[[2]]*100
-            ) %>%
-    ggplot(aes(NAME_CONTRACT_TYPE, frac_def, fill = NAME_CONTRACT_TYPE)) + 
-    geom_col() + 
-    geom_errorbar(aes(ymin = lwr, ymax = upr), width = 0.5, size = 0.7, color = 'gray30') + 
-    theme(legend.position = "none") + labs(y = 'Default rate [%]')
-# Cash loans have higher chance of default
-
-# CODE_GENDER
-p2 = application_train %>%
-    group_by(CODE_GENDER, TARGET) %>%
-    count() %>%
-    spread(TARGET, n) %>%
-    mutate(frac_def = `1`/(`1`+`0`)*100
-            # lwr = get_binCI(`1`,(`1`+`0`))[[1]]*100
-            # upr = get_binCI(`1`,(`1`+`0`))[[2]]*100
-            ) %>%
-    ggplot(aes(CODE_GENDER, frac_def, fill = CODE_GENDER)) + 
-    geom_col()
-    #geom_errorbar(aes(ymin = lwr, ymax = upr), width = 0.5, size = 0.7, color = 'gray30') + 
-    theme(legend.position = "none") + labs(y = 'Default rate [%]')
-# Males have a higher propensity to default
-
-# FLAG_OWN_CAR
-p3 = application_train %>%
-    group_by(FLAG_OWN_CAR, TARGET) %>%
-    count() %>%
-    spread(TARGET, n) %>%
-    mutate(frac_def = `1`/(`1`+`0`)*100,
-            lwr = get_binCI(`1`,(`1`+`0`))[[1]]*100,
-            upr = get_binCI(`1`,(`1`+`0`))[[2]]*100
-            ) %>%
-    ggplot(aes(FLAG_OWN_CAR, frac_def, fill = FLAG_OWN_CAR)) + 
-    geom_col() + 
-    geom_errorbar(aes(ymin = lwr, ymax = upr), width = 0.5, size = 0.7, color = 'gray30') + 
-    theme(legend.position = "none") + labs(y = 'Default rate [%]')
-# People owning a car have a lower rate of default
-
-# FLAG_OWN_REALTY
-p4 = application_train %>%
-    group_by(FLAG_OWN_REALTY, TARGET) %>%
-    count() %>%
-    spread(TARGET, n) %>%
-    mutate(frac_def = `1`/(`1`+`0`)*100,
-            lwr = get_binCI(`1`,(`1`+`0`))[[1]]*100,
-            upr = get_binCI(`1`,(`1`+`0`))[[2]]*100
-            ) %>%
-    ggplot(aes(FLAG_OWN_REALTY, frac_def, fill = FLAG_OWN_REALTY)) + 
-    geom_col() + 
-    geom_errorbar(aes(ymin = lwr, ymax = upr), width = 0.5, size = 0.7, color = 'gray30') + 
-    theme(legend.position = "none") + labs(y = 'Default rate [%]')
-# People owning realty have slightly lower default rate
-
-# Integer features
-
-# CNT_CHILDREN
-p5 = application_train %>%
-    mutate(CNT_CHILDREN = ifelse(CNT_CHILDREN < 5, CNT_CHILDREN, 5)) %>%
-    group_by(CNT_CHILDREN, TARGET) %>%
-    count() %>%
-    spread(TARGET, n) %>%
-    mutate(frac_def = `1`/(`1`+`0`)*100
-            #lwr = get_binCI(`1`,(`1`+`0`))[[1]]*100,
-            #upr = get_binCI(`1`,(`1`+`0`))[[2]]*100
-            ) %>%
-    ggplot(aes(CNT_CHILDREN, frac_def)) + 
-    geom_point() + 
-    #geom_errorbar(aes(ymin = lwr, ymax = upr), width = 0.5, size = 0.7, color = 'gray30') + 
-    theme(legend.position = "none") + labs(y = 'Default rate [%]')
-table(application_train$CNT_CHILDREN)
-# Default rte is observed to increase with increasing children
-
-# Float features
-
-# AMT_INCOME_TOTAL
-
-application_train$AMT_INCOME_TOTAL = ifelse(application_train$AMT_INCOME_TOTAL < quantile(application_train$AMT_INCOME_TOTAL, 0.01),
-                                            quantile(application_train$AMT_INCOME_TOTAL, 0.01),
-                                            ifelse(application_train$AMT_INCOME_TOTAL > quantile(application_train$AMT_INCOME_TOTAL, 0.99),
-                                                   quantile(application_train$AMT_INCOME_TOTAL, 0.99),
-                                                   application_train$AMT_INCOME_TOTAL))
-
-p6 = application_train %>%
-    ggplot(aes(AMT_INCOME_TOTAL, fill = TARGET)) +
-    geom_density(alpha = 1, bw = 0.01) + 
-    theme(legend.position = "none")
-    
-    AMT_CREDIT
-application_train %>%
-    ggplot(aes(AMT_CREDIT, fill = TARGET)) +
-    geom_density(alpha = 1, bw = 0.1) + 
-    theme(legend.position = "none")
-    
-application_train %>%
-
-  ggplot(aes(AMT_CREDIT, fill = TARGET)) +
-
-  geom_density(alpha = 0.5, bw = 0.05) +
-
-  theme(legend.position = "none")
+multiplt_text = "multiplot("
+for (plot in unlist(ls()[as.numeric(lapply(ls(), function(x) grep("plot_", x))) == 1])) {
+  if(!is.na(plot)){
+    # print(plot)
+    multiplt_text = paste0(multiplt_text, plot, ",")
+  }
+}
+multiplt_text <- paste0(substr(multiplt_text, 1, nchar(multiplt_text) - 1), ", cols = 4)")
+eval(parse(text = multiplt_text))
