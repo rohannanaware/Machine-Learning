451b1fd94d96cd17647668dad2c425b328739605
Rohan M. Nanaware <rohan.nanaware@outlook.com>
2021-01-15 19:18:58 +0530

    Update pick per face.py

--- FK/pick per face/pick per face.py
+++ FK/pick per face/pick per face.py
@@ -92,85 +92,60 @@ for index, row in input_data_sim.iterrows():
 ulub_bts_plsize = pd.read_csv('ulub_bts_plsize.csv')
 ulub_bts_plsize['pickzone'] = ulub_bts_plsize['pickzone'].str.lower()
 ulub_bts_plsize.drop('fc',axis = 1,inplace = True)
-input_data_sim['picking_zone'] = input_data_sim['picking_zone'].str.lower()
+#input_data_sim.drop(['pickzone','pl_max','picking_zone_sim','storage_location_label_sim'],axis = 1,inplace = True)
+#lookup the pickzones for the assigned bins
+location_dim = input_data_sim.groupby(['picking_zone','storage_location_label'],as_index=False).agg({'fulfill_reference_id':'count'})
+location_dim.sort_values(['storage_location_label','fulfill_reference_id'],ascending=[0,0],inplace = True)
+location_dim.drop_duplicates(subset = 'storage_location_label',keep='first', inplace=True)
+location_dim.drop('fulfill_reference_id',axis = 1,inplace = True)
+location_dim.columns = ['picking_zone_sim','storage_location_label_sim']
+input_data_sim = pd.merge(input_data_sim,
+                           location_dim,
+                           left_on='bin_assigned',
+                           right_on='storage_location_label_sim',
+                           how='left')
+# input_data_sim.isna().sum()
+input_data_sim['picking_zone_sim'] = input_data_sim['picking_zone_sim'].str.lower()
 input_data_sim = pd.merge(input_data_sim,
                           ulub_bts_plsize,
                           how='left',
-                          left_on='picking_zone',
+                          left_on='picking_zone_sim',
                           right_on='pickzone')
 input_data_sim.loc[input_data_sim['pickzone'].isna(),'pl_max'] = 12
 
-output_data = pd.DataFrame()
-#output_data.columns = input_data_sim.columns
-loop_counter = 0
-for pz in input_data_sim.picking_zone.unique():
-    
-    loop_data = input_data_sim[input_data_sim['picking_zone'] == pz]
-    loop_data.sort_values(by='order_item_approved_timestamp',
-                          ascending = True,inplace = True)
-    loop_data.reset_index(inplace = True, drop = True)
-    pl_id_counter = 1
-    pl_size = 0
-    for index, row in loop_data.iterrows():
-        
-        if loop_counter % 1000 == 0:
-            print(loop_counter)
-            
-        if pl_size < row['pl_max']:
-            # add more items to the pl
-            loop_data[index,'pl_id_sim'] = str(pl_id_counter) + '_' + pz
-            pl_size = pl_size + 1
-        else:
-            # create a new pl
-            pl_id_counter = pl_id_counter + 1
-            pl_size = 0
-        output_data = output_data.append(loop_data)
-        loop_counter = loop_counter + 1
-
 output_data_sim = pd.DataFrame()
-for pz in input_data_sim.picking_zone.unique():
+for pz in input_data_sim.picking_zone_sim.unique():
     
-    loop_data = input_data_sim[input_data_sim['picking_zone'] == pz]
-    loop_data.sort_values(by='order_item_approved_timestamp',
+    loop_data = input_data_sim[input_data_sim['picking_zone_sim'] == pz]
+    loop_data.sort_values(by='reservation_created_timestamp',
                           ascending = True,inplace = True)
     loop_data.reset_index(inplace = True, drop = True)
     loop_data.reset_index(inplace = True, drop = False)
     pl_max = max(loop_data.pl_max)
     loop_data['picklist_id_sim'] = (loop_data['index'] // pl_max).astype(int).astype(str) + '_' + pz
-    loop_data['picklist_id_sim'].value_counts()
-    
     print('pickzone {} ran | cumulative reservations of {} %'.format(pz,round((output_data_sim.shape[0] + loop_data.shape[0])/input_data_sim.shape[0]*100,2)))
     output_data_sim = output_data_sim.append(loop_data)
-
-# benchmarks
-input_data_sim_agg = input_data_sim.groupby(['picklist_display_id'],as_index=False).agg(
-        {'fulfill_reference_id':'count',
+        
+# benchmark metrics : as is
+input_data_sim_agg = input_data_sim.groupby(['picking_zone','picklist_display_id'],as_index=False).agg(
+        {'fulfill_reference_id':'nunique',
          'storage_location_label':'nunique'})
-input_data_sim_agg['pick_per_face_asis'] = input_data_sim_agg['fulfill_reference_id']/input_data_sim_agg['storage_location_label']
-input_data_sim_agg['pick_per_face_asis'].agg({np.mean,np.median})
-# mean      1.265207 median    1.000000
+input_data_sim_agg['pick_per_face'] = input_data_sim_agg['fulfill_reference_id']/input_data_sim_agg['storage_location_label']
+input_data_sim_agg['pick_per_face'].agg({np.mean,np.median})
+# mean      2.56 median    1.7
 input_data_sim_agg['fulfill_reference_id'].agg({np.mean,np.median})
-# mean      2.432859 median    2.000000
+# mean      9.8 median    9
 
-# simulation results
-output_data_sim_agg = output_data_sim.groupby(['picklist_id_sim'],as_index=False).agg(
-        {'fulfill_reference_id':'count',
+# benchmark metrics : simulation
+output_data_sim_agg = output_data_sim.groupby(['picking_zone_sim','picklist_id_sim'],as_index=False).agg(
+        {'fulfill_reference_id':'nunique',
          'bin_assigned':'nunique'})
-output_data_sim_agg['pick_per_face_asis'] = output_data_sim_agg['fulfill_reference_id']/input_data_sim_agg['storage_location_label']
-output_data_sim_agg['pick_per_face_asis'].agg({np.mean,np.median})
-# mean      7.713186 median    6.000000
+output_data_sim_agg['pick_per_face'] = output_data_sim_agg['fulfill_reference_id']/input_data_sim_agg['storage_location_label']
+output_data_sim_agg['pick_per_face'].agg({np.mean,np.median})
+# mean      4.15 median    2.4
 output_data_sim_agg['fulfill_reference_id'].agg({np.mean,np.median})
-# mean      10.920091 median    12.000000
+# mean      11.78 median    12
 
-
-
-
-
-# qc
-temp1 = inventory_binwid_lvl.loc[(inventory_binwid_lvl['wid'] == row['wid']) & (inventory_binwid_lvl['storage_location_label'].isin(bin_label)),:]
-temp2 = inventory_binwid_lvl.loc[(inventory_binwid_lvl['wid'] == row['wid']),:]
-temp3 = inventory_binwid_lvl.loc[(inventory_binwid_lvl['storage_location_label'].isin(bin_label)),:]
-
-temp4 = inventory_binwid_lvl.loc[(inventory_binwid_lvl['wid'] == row['wid']) & (inventory_binwid_lvl['storage_location_label'].isin(bin_label)),:]
-temp5 = inventory_binwid_lvl.loc[(inventory_binwid_lvl['wid'] == row['wid']),:]
-temp6 = inventory_binwid_lvl.loc[(inventory_binwid_lvl['storage_location_label'].isin(bin_label)),:]
+# export data
+input_data_sim_agg.to_csv('input_data_sim_agg.csv')
+output_data_sim_agg.to_csv('output_data_sim_agg.csv')
